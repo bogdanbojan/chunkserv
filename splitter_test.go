1cc090f83a60e3a33e24dd83271d268abfb85af2
bogdanbojan <bogdanbojan03@gmail.com>
2022-02-01 15:01:59 +0200

    refactored splitter_test - removed some redundant comments

--- splitter_test.go
+++ splitter_test.go
@@ -18,26 +18,8 @@ func TestSplitLine(t *testing.T) {
 		testLine := "1|Mavra|Malec|mmalec0@usa.gov|Female|229.215.245.102"
 		got := splitLine(testLine)
 		want := Lines{"1", "Mavra", "Malec", "mmalec0@usa.gov", "Female", "229.215.245.102"}
-
 		assertEqual(t, got, want)
 	})
-	//if !isEqual(got, want) {
-	//	t.Errorf("got %q want %q", got, want)
-	//}
-
-	//if got != want {
-	//	t.Errorf("got %q want %q", got, want)
-	//}
-
-}
-
-func isEqual(line1 Lines, line2 Lines) bool {
-	for i, v := range line1 {
-		if v != line2[i] {
-			return false
-		}
-	}
-	return true
 }
 
 func TestIsValid(t *testing.T) {}
